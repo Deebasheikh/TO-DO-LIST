712b81104f7ed5b92472947daa3306d2999c8ebe
Areeba Sheikh <areebasheikh27.as@gmail.com>
2024-04-04 00:50:55 -0700

    created cli-to-do-list

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TO-DO-LIST
-This is a to do list.
+Introducing "TO-DO-LIST app" - your command-line interface (CLI) companion for efficient task management. This app is a minimalist yet powerful to-do list application designed to seamlessly integrate into your workflow and enhance productivity without distractions.
+
+Key Features:
+
+Add Task: Quickly add tasks to your to-do list using simple commands. Just click on "Addtask" to capture new tasks on the fly.
+
+View: Easily review your entire to-do list with a single command. Use "view" to display all your tasks, sorted by priority or due date, directly in your terminal window.
+
+Update: Stay organized and flexible by updating task details as needed. Modify task descriptions, due dates, priorities, and more with the update command, ensuring your to-do list remains accurate and relevant.
+
+Delete: Streamline your task management process by removing completed or unnecessary tasks with ease. Simply use the  delete command to eliminate tasks from your list and keep it clutter-free.
+
+Exit: When you're finished managing your tasks, exit  smoothly with the exit command. It respects your time and ensures a seamless transition back to your other tasks and activities.
+
+ It is designed for users who prefer a command-line interface for task management, providing a distraction-free environment while offering essential features to keep you organized and productive.
+
+Experience the simplicity and efficiency of task management with this app. Install it today and take control of your to-do list right from your terminal!
---
+++ main.ts
@@ -0,0 +1,127 @@
+#! /usr/bin/env node
+
+//todolistapp
+//array
+//operation
+//function
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos: string[] = [];
+let condition = true;
+async function maketodo(todos: string[]) {
+  do {
+    console.log(chalk.blue.bold.italic("WELCOME TO THE TODO LIST APP"));
+
+    let operation = await inquirer.prompt([
+      {
+        name: "operation",
+        message: "What would you like to do? \n Please select.",
+        type: "list",
+        choices: ["Addtask", "View", "Update", "Delete", "Exit"],
+      },
+    ]);
+
+    if (operation.operation === "Addtask") {
+      let addTask = await inquirer.prompt([
+        {
+          name: "todo",
+          type: "input",
+          message: "What would you like to add in your Todos?",
+        },
+        {
+          name: "addMore",
+          type: "confirm",
+          message: "would you like to addmore in your todos?",
+          default: true,
+        },
+        {
+          name: "todo1",
+          type: "input",
+          message: "What would you like to add in your Todos?",
+        },
+      ]);
+      todos.push(addTask.todo);
+      condition = addTask.addMore;
+      todos.push(addTask.todo1);
+    }
+    if (operation.operation === "View") {
+      let View = await inquirer.prompt({
+        name: "view",
+        type: "confirm",
+        message: "Would you like to view your todo list?",
+        default: true,
+      });
+      condition = View.view
+      console.log(chalk.bgMagenta.white.bold("***YOUR TODO LIST***"));
+      todos.forEach((todos) => console.log(chalk.magenta.bold.italic(todos)));
+    }
+    if (operation.operation === "Update") {
+      let Update = await inquirer.prompt([
+        {
+          name: "update",
+          type: "confirm",
+          message: "Would you like to update your todo list?",
+          default: true,
+        },
+
+        {
+          name: "updatetask",
+          type: "list",
+          message: "Select task to update",
+          choices: todos,
+        },
+      ]);
+      let update2 = await inquirer.prompt([
+        {
+          name: "updatetask2",
+          type: "input",
+          message: "update task",
+        },
+      ]);
+
+      condition = Update.update;
+      let newtodos = todos.filter((val) => val != Update.updatetask);
+      todos = [...newtodos, update2.updatetask2];
+      console.log(chalk.bgMagenta.white.bold("***UPDATED TODO LIST***"));
+      todos.forEach((todos) => console.log(chalk.red.bold(todos)));
+    }
+    if (operation.operation === "Delete") {
+      let remove = await inquirer.prompt([
+        {
+          name: "delete",
+          type: "confirm",
+          message: "Would you like to delete task from your todo list?",
+          default: true,
+        },
+        {
+          name: "removetask",
+          type: "list",
+          message: "Delete the task",
+          choices: todos,
+        },
+      ]);
+      condition = remove.delete;
+      let newtodos = todos.filter((val) => val != remove.removetask);
+      todos = [...newtodos];
+      console.log(chalk.bgRed.white.bold("***Deleted todo task***"));
+      todos.forEach((todos) => console.log(chalk.redBright.bold.italic(todos)));
+    }
+    if (operation.operation === "Exit") {
+      let Exit = await inquirer.prompt({
+        name: "exit",
+        type: "confirm",
+        message: "Would you like to exit from the TODO APP?",
+        default: true,
+      });
+      condition = Exit.exit;
+      console.log(
+        chalk.bgGreen.white.bold.italic(
+          "Thankyou for using the TODO APP.Have a great day!"
+        )
+      );
+      break;
+    }
+  } while (true);
+}
+
+maketodo(todos);
